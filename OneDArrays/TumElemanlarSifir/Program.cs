e21664d6af0aba712c782a74eb2f9d95342ae245
berkaykarakush <berkaykaraks@outlook.com>
2023-12-05 13:49:20 +0300

    Added - TumElemanlarSifir

---
+++ OneDArrays/TumElemanlarSifir/Program.cs
@@ -0,0 +1,11 @@
+﻿// dizinin eleman sayisi
+Console.Write("Eleman sayisini giriniz: ");
+int n = int.Parse(Console.ReadLine());
+// n elemanli dizi olustur
+int[] dizi = new int[n];
+//tum elemanlara sifir degerini at
+for (int i = 0; i < dizi.Length; i++)
+    dizi[i] = 0;
+//elemanlari yazdir
+for (int i = 0;i<=dizi.Length;i++)
+    Console.WriteLine($"{i}.Eleman : {dizi[i]}");
