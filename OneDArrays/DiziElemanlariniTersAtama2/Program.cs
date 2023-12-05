da5e01081d7e649f894c88a9bf84dbdd3046b849
berkaykarakush <berkaykaraks@outlook.com>
2023-12-05 15:03:58 +0300

    Added - DiziElemanlariniTersAtama2

---
+++ OneDArrays/DiziElemanlariniTersAtama2/Program.cs
@@ -0,0 +1,22 @@
+﻿// dizi elemanlarini baska dizi kullanmadan tersten yazdirma
+Console.Write("Dizinin eleman sayisini giriniz: ");
+int n = int.Parse(Console.ReadLine());
+// n elemanli dizi tanimla
+int[] array = new int[n];
+// dizinin elemanlarini al
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"{i+1}.Eleman: ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+// diziyi ters cevir
+for (int i = 0; i < n / 2; i++)
+{
+    int r = array[i];
+    array[i] = array[n-1-i];
+    array[n - 1 - i] = r;
+}
+// tersten cikti
+for (int i = 0; i < n; i++)
+    Console.WriteLine($"{i+1}.Eleman: {array[i]}");
+
---
+++ OneDArrays/DizilerinSkalerCarpimi/Program.cs
@@ -0,0 +1,21 @@
+﻿// n elemanli a ve b dizilerinin skaler carpimi
+Console.Write("Eleman sayisini giriniz: ");
+int n = int.Parse(Console.ReadLine());
+// n elemanli diziler
+int[] array = new int[n];
+int[] array2 = new int[n];
+int result = 0;
+for (int i = 0; i < n; i++)
+{
+    // 1. dizinin elemanlarini al
+    Console.Write($"1. dizinin {i+1}.Elemani: ");
+    array[i] = int.Parse(Console.ReadLine());
+    // 2. dizinin elemanlarini al
+    Console.Write($"2. dizinin {i+1}.Elemani: ");
+    array2[i] = int.Parse(Console.ReadLine());
+
+    // onceki toplam + 1. eleman * 2. eleman seklinde hesaplama
+    result += array[i] * array2[i];
+}
+// output
+Console.WriteLine($"Skaler Carpim: {result}");
