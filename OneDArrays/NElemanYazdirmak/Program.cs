79284b896efa406142339d0f4cdcf1344b1fe871
berkaykarakush <berkaykaraks@outlook.com>
2023-12-05 13:42:25 +0300

    Added - NElemanYazdirmak

--- OneDArrays/NElemanYazdirmak/Program.cs
+++ OneDArrays/NElemanYazdirmak/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Eleman sayisi:");
+//girilen degeri al
+int n = int.Parse(Console.ReadLine());
+// dizi tanimla
+int[] sayilar = new int[n];
+// n adet donecek ve girilen sayilari diziye ekleyekctir
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"1.sayi {i + 1}:");
+    sayilar[i] = int.Parse(Console.ReadLine());
+}
+//sayilari goster
+for (int i = 0; i < sayilar.Length; i++)
+    Console.WriteLine($"{i + 1}. Eleman: {sayilar[i]}");
