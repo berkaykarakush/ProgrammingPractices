7ce1212402c057c0a2ab5c38cec0203ae2d38978
berkaykarakush <berkaykaraks@outlook.com>
2023-12-05 14:19:18 +0300

    Added - DizininkileCarpimi

---
+++ OneDArrays/DizininkileCarpimi/Program.cs
@@ -0,0 +1,20 @@
+﻿// eleman sayisi ve katsayiyi al
+Console.Write("Eleman sayisi: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Katsayi: ");
+int k = int.Parse(Console.ReadLine());
+
+// n elemanl dizi tanimla
+int[] array = new int[n];
+int[] array2 = new int[n];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{i+1}. Eleman: ");
+    // dizi elemanlarini al
+    array[i] = int.Parse(Console.ReadLine());
+    // k ile dizinin elemanlarini carp ve yeni diziye ata
+    array2[i] = array[i] * k;
+}
+// output
+for (int i = 0;i < array2.Length; i++)
+    Console.WriteLine($"{array2[i]}");
