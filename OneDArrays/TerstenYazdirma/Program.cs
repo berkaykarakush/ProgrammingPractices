f2643ae6c47cc23b5bfab202aea42b9b0cdf966f
berkaykarakush <berkaykaraks@outlook.com>
2023-12-06 16:39:37 +0300

    Added - TerstenYazdirma

---
+++ OneDArrays/TerstenYazdirma/Program.cs
@@ -0,0 +1,7 @@
+﻿// girdi
+Console.Write("Cumle: ");
+string sentence = Console.ReadLine();
+Console.Write("Cumlenin Tersten Yazimi: ");
+// cumlenin sonundan basina dogru dongu
+for (int i = sentence.Length-1; i >= 0; i--)
+    Console.Write($"{sentence[i]}");
