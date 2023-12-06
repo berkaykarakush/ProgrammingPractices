fc4d8e9e7427e378a3b073f6fad3533c3217f539
berkaykarakush <berkaykaraks@outlook.com>
2023-12-06 15:42:38 +0300

    Added - BasilanTusunTuru

---
+++ OneDArrays/BasilanTusunTuru/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+// girdi
+Console.Write("input: ");
+string input = Console.ReadLine();
+// alinan degeri ascii koduna donusturen method
+var bytes = ASCIIEncoding.ASCII.GetBytes(input);
+// cikti
+foreach (var b in bytes)
+    Console.WriteLine(b);
