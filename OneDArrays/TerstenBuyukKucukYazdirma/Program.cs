b949ed9fdb84ec2c396b9008ab4cc0ff23cc4028
berkaykarakush <berkaykaraks@outlook.com>
2023-12-06 16:59:00 +0300

    Added - TerstenBuyukKucukYazdirma

---
+++ OneDArrays/TerstenBuyukKucukYazdirma/Program.cs
@@ -0,0 +1,12 @@
+﻿// girdi
+Console.Write("Cumle: ");
+string sentence = Console.ReadLine();
+// cikti
+Console.Write("Tersten: ");
+// islemin yapildigi dongu
+for (int i = sentence.Length - 1; i >= 0; i--)
+{
+    // cift ise lower, tek ise upper
+    char character =(i % 2 == 0) ? char.ToLower(sentence[i]) : char.ToUpper(sentence[i]);
+    Console.Write(character);
+}
