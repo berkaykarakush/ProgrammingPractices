0aa05d52656348d2429514df5fad99e7d8ce6030
berkaykarakush <berkaykaraks@outlook.com>
2023-12-06 12:02:25 +0300

    Added - SayidanKelimeye

---
+++ OneDArrays/SayidanKelimeye/Program.cs
@@ -0,0 +1,24 @@
+﻿// sayilarin isim karsiliklarinin tutuldugu dizi
+string[] numbers = new string[10];
+numbers[0] = "zero";
+numbers[1] = "one";
+numbers[2] = "two";
+numbers[3] = "three";
+numbers[4] = "four";
+numbers[5] = "five";
+numbers[6] = "six";
+numbers[7] = "seven";
+numbers[8] = "eight";
+numbers[9] = "nine";
+// sayi girdisi
+Console.Write("enter the number -9 to 9: ");
+int number = int.Parse(Console.ReadLine());
+// pozitif cikti
+if (number > 0)
+    Console.WriteLine($"{numbers[Math.Abs(number)]}");
+// sifir ise
+else if(number == 0)
+    Console.WriteLine($"{numbers[Math.Abs(number)]}");
+// negatif cikti
+else
+    Console.WriteLine($"minus {numbers[Math.Abs(number)]}");
