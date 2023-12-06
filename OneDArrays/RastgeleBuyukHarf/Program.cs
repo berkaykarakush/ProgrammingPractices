8e59aa0b5ce258e37df68be2c4dac9c4f886af85
berkaykarakush <berkaykaraks@outlook.com>
2023-12-06 17:31:00 +0300

    Added - RastgeleBuyukHarf

---
+++ OneDArrays/RastgeleBuyukHarf/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Cumle: ");
+// Girdi
+string sentence = Console.ReadLine();
+Console.Write("Tersten: ");
+for (int i = sentence.Length - 1; i >= 0; i--)
+{
+    // rastgele sayi uret
+    int random = new Random().Next();
+    // cift ise buyut, tek ise kucult
+    char charachter = (random % 2 == 0) ? char.ToUpper(sentence[i]) : char.ToLower(sentence[i]);
+    Console.Write(charachter);
+}
