b626e4a2f129618c4630c8ae5c4dce4e23dcd34a
berkaykarakush <berkaykaraks@outlook.com>
2023-12-06 17:49:47 +0300

    Added - BuyukseKucukKucukseBuyuk

---
+++ OneDArrays/BuyukseKucukKucukseBuyuk/Program.cs
@@ -0,0 +1,11 @@
+﻿// girdi
+Console.Write("Cumle: ");
+string sentence = Console.ReadLine();
+
+// yeni degeri tutacak degisken
+string result = "";
+foreach (var s in sentence)
+    // sayi buyukse kucult, kucukse buyut
+    result += char.IsUpper(s) ? char.ToLower(s) : char.ToUpper(s);
+// cikti
+Console.WriteLine($"Yeniden: {result}");
