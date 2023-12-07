6755b311e96ea6667db5d3a9ec0247a9c0ed9dd6
berkaykarakush <berkaykaraks@outlook.com>
2023-12-07 16:04:54 +0300

    Added - SonHarfleriDegistirme

---
+++ OneDArrays/SonHarfleriDegistirme/Program.cs
@@ -0,0 +1,9 @@
+﻿// girdi
+Console.Write("Cumle: ");
+string sentence = Console.ReadLine().ToLower();
+Console.Write("Gizlenecek sondan karakter sayisi: ");
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < sentence.Length; i++)
+    // i degeri kucukse acik harf, i degeri buyuk esitse acik harf gozukecektir
+    Console.Write((i >= sentence.Length - count) ? "*" : sentence[i]) ;
