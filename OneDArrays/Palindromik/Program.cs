ed38aa5ca3a06f061e329653ea4abf49463e3156
berkaykarakush <berkaykaraks@outlook.com>
2023-12-07 16:59:08 +0300

    Added - Palindromik

---
+++ OneDArrays/Palindromik/Program.cs
@@ -0,0 +1,9 @@
+﻿// girdi
+Console.Write("Cumle: ");
+string sentence = Console.ReadLine().ToLower();
+string reverse =  "";
+// cumlenin tersini al
+for (int i = sentence.Length - 1; i >= 0; i--)
+    reverse += sentence[i];
+// cikti
+Console.WriteLine(sentence == reverse ? "Palindrom":"Palindrom degil");
