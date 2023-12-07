4814f1ec008c520d203979af3ae6da58cb0a7a03
berkaykarakush <berkaykaraks@outlook.com>
2023-12-07 16:35:59 +0300

    Added - KelimeSayisi

---
+++ OneDArrays/KelimeSayisi/Program.cs
@@ -0,0 +1,9 @@
+﻿// girdi
+Console.Write("Cumle: ");
+string sentence = Console.ReadLine().ToLower();
+// virgulleri bosluk ile degistir
+sentence = sentence.Replace(',', ' ');
+// bosluk karakterlerinden ayirma islemi gerceklestir
+string[] words = sentence.Split(" ");
+// kelime sayisi
+Console.Write($"Kelime Sayisi: {words.Length}");
