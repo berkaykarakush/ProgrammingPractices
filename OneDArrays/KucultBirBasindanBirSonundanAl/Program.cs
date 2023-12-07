cd435ddd1e66ca20168feed818be23790d2212c0
berkaykarakush <berkaykaraks@outlook.com>
2023-12-07 15:49:10 +0300

    Added - KucultBirBasindanBirSonundanAl

---
+++ OneDArrays/KucultBirBasindanBirSonundanAl/Program.cs
@@ -0,0 +1,10 @@
+﻿// girdi
+Console.Write("Cumle buyuk harflerle: ");
+// cumleyi kucult
+string sentence = Console.ReadLine().ToLower();
+// tersten
+for (int i = sentence.Length - 1; i >= 0; i--)
+    Console.Write(sentence[i]);
+// normal
+for (int i = 0; i < sentence.Length; i++)
+    Console.Write(sentence[i]);
