3342cca22ff4b0b145ef68039ac8759bdd155183
berkaykarakush <berkaykaraks@outlook.com>
2023-12-07 17:18:56 +0300

    Added - SayiyiBasamaklarinaAyirmak

---
+++ OneDArrays/SayiyiBasamaklarinaAyirmak/Program.cs
@@ -0,0 +1,11 @@
+﻿// girdi
+Console.Write("Sayi giriniz: ");
+string input = Console.ReadLine();
+// baslik
+Console.WriteLine("Basamaklar\n---------------");
+// sayiyi parcala
+for (int i = input.Length - 1;i >= 0; i--)
+{
+    // basamaklarina gore basamak degerini yaz
+    Console.WriteLine($"{Math.Pow(10, input.Length - 1 - i)} => {input[i]}");
+}
