367b3a0e971ad475578faef3d9f7ff2b531527ce
berkaykarakush <berkaykaraks@outlook.com>
2023-12-07 12:16:20 +0300

    Added - KelimeleriSifreleme

---
+++ OneDArrays/KelimeleriSifreleme/Program.cs
@@ -0,0 +1,11 @@
+﻿// girdi
+Console.Write("Cumle: ");
+string sentence = Console.ReadLine().ToLower();
+// cumle icinde dolas
+for (int i = 0; i < sentence.Length; i++)
+{
+    // -87 ile sifrele ve int'e donustur
+    int s = sentence[i] - 87;
+    // int/sifreli degerini goster
+    Console.Write($"{s} = ");
+}
