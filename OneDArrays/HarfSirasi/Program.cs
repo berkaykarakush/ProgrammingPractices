1555020071402d6c5d8e07ae5a96b1cccbae1609
berkaykarakush <berkaykaraks@outlook.com>
2023-12-07 12:06:17 +0300

    Added - HarfSirasi

---
+++ OneDArrays/HarfSirasi/Program.cs
@@ -0,0 +1,6 @@
+﻿// girdi
+Console.Write("Cumle: ");
+string sentence = Console.ReadLine().ToLower();
+// cumle icinde gezelim
+for (int i = 0; i < sentence.Length; i++)
+    Console.Write($"{sentence[i]} : {i+1}\t");
--- OneDArrays/SessizSesliKelimeOlusturma/Program.cs
+++ OneDArrays/SessizSesliKelimeOlusturma/Program.cs
@@ -9,16 +9,21 @@ string sentenceV = "", sentenceC = "";
 for (int i = 0; i < sentence.Length; i++)
 {
 	bool isVowel = false;
+	// cumle icinde dolas
 	for (int j = 0; j < vowels.Length; j++)
 	{
+		// esitse
 		if (sentence[i] == vowels[j])
 		{
+			// sesliye ekle
             sentenceV += sentence[i];
 			isVowel = true;
 			break;
         }
     }
+	// sessize ekle
 	if(!isVowel)
         sentenceC += sentence[i];
 }
+// cikti
 Console.WriteLine($"Sesli: {sentenceV} \nSessiz: {sentenceC}");
