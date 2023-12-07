27cc85aa8e32cdd0ffce66d1c440fce7e4064162
berkaykarakush <berkaykaraks@outlook.com>
2023-12-07 15:33:32 +0300

    Added - SifredenCumleye

---
+++ OneDArrays/SifredenCumleye/Program.cs
@@ -0,0 +1,7 @@
+﻿// girdi
+Console.Write("Sifreli cumle: ");
+string sentence = Console.ReadLine();
+for (int i = 0; i < sentence.Length; i++)
+    // cift ise sonraki harfi tek ise onceki harfi getir 
+    Console.Write((i % 2 == 0) ? sentence[i+1] : sentence[i-1]);
+
