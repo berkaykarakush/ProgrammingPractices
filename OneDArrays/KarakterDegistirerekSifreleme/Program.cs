4e2150d67e376930e46c7c1535846b723fd3f383
berkaykarakush <berkaykaraks@outlook.com>
2023-12-07 14:57:34 +0300

    Added - KarakterDegistirerekSifreleme

---
+++ OneDArrays/KarakterDegistirerekSifreleme/Program.cs
@@ -0,0 +1,10 @@
+﻿// girdi
+Console.Write("Cumle: ");
+string sentence = Console.ReadLine().ToLower();
+// tek ise, bosluk ekleyerek cift olmasi saglar
+sentence += (sentence.Length % 2 != 0) ? " " : "";
+// islemin yapildigi dongu
+for (int i = 0; i < sentence.Length; i++)
+    // i cift ise i'den sonraki harfi getir, i tek ise i'den onceki harfi getir
+    Console.Write((i % 2 == 0) ? sentence[i+1] : sentence[i -1]);
+
