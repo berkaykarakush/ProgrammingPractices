c565a0a59323c764c1ee504653f6da8b886af48c
berkaykarakush <berkaykaraks@outlook.com>
2023-12-07 16:47:18 +0300

    Added - AHarfiSayaci

---
+++ OneDArrays/AHarfiSayaci/Program.cs
@@ -0,0 +1,14 @@
+﻿// icinde a harfi olan kelime sayisi
+Console.Write("Cumle: ");
+// cumleyi al icinde , ya da . varsa ' ' ile degistir ve kucult
+string sentence = Console.ReadLine().ToLower().Replace(',',' ').Replace('.', ' ');
+// bosluklardan ayir ayrica bos karakterleri sil diziye ata
+string[] words =  sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int counter = 0;
+// dizi icinde don
+for (int i = 0; i < words.Length; i++)
+    // a iceren kelime varsa sayaci arttir
+    if (words[i].Contains("a"))
+        counter++;
+// sonuc
+Console.WriteLine($"Icinde a gecen kelime sayisi: {counter}");
--- OneDArrays/KelimeSayisi/Program.cs
+++ OneDArrays/KelimeSayisi/Program.cs
@@ -2,8 +2,8 @@
 Console.Write("Cumle: ");
 string sentence = Console.ReadLine().ToLower();
 // virgulleri bosluk ile degistir
-sentence = sentence.Replace(',', ' ');
+sentence = sentence.Replace(',', ' ').Replace('.', ' ');
 // bosluk karakterlerinden ayirma islemi gerceklestir
-string[] words = sentence.Split(" ");
+string[] words = sentence.Split(" ", StringSplitOptions.RemoveEmptyEntries);
 // kelime sayisi
 Console.Write($"Kelime Sayisi: {words.Length}");
