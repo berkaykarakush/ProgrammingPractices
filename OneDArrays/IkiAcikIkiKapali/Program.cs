6e13796d512245e01c577b84561a8455c4196ff4
berkaykarakush <berkaykaraks@outlook.com>
2023-12-07 15:39:37 +0300

    Added - IkiAcikIkiKapali

---
+++ OneDArrays/IkiAcikIkiKapali/Program.cs
@@ -0,0 +1,7 @@
+﻿// girdi
+Console.Write("Cumle :");
+// tum kelimeyi buyuk olarak al, sonradan buyutmek icin ugrasma
+string sentence = Console.ReadLine().ToUpper();
+for (int i = 0; i < sentence.Length; i++)
+    // 2'den kucukse ilk 2, 2'den buyuksek sonraki 2 anlaminda
+    Console.Write((i % 4 < 2) ? sentence[i] :"*");
