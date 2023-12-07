10198ab58f96dd0d7ba72fb4570cfca8d2a876d5
berkaykarakush <berkaykaraks@outlook.com>
2023-12-07 11:42:09 +0300

    Added - SesliHarfBulma

---
+++ OneDArrays/SesliHarfBulma/Program.cs
@@ -0,0 +1,27 @@
+﻿// sesli harfler
+char[] letters = {'a','e','i', 'o', 'u'};
+// girdi
+Console.Write("Cumleyi giriniz: ");
+string sentence = Console.ReadLine().ToLower();
+int counter = 0;
+// yerlerin tutulacagi dizi
+int[] places = new int[sentence.Length];
+// islemlerin yapildigi dongu
+for (int i = 0; i < sentence.Length; i++)
+{
+    // harf karsilastirmasi
+    for (int j = 0; j < letters.Length; j++)
+    {
+        // dogru ise sayaci arttir ve yer bilgisini al
+        if (sentence[i] == letters[j])
+        {
+            counter++;
+            places[counter] = i+1;
+        }
+    }
+}
+Console.WriteLine($"{counter} tane sesli harf vardir. \nYerleri: ");
+// yer bilgisini yazdir
+for (int i = 0; i <= counter; i++)
+    if (places[i] != 0)
+        Console.Write($"{places[i]} ");
