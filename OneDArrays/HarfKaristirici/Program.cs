2ea83d66280cf271ec387a7dd5520b543b41a78f
berkaykarakush <berkaykaraks@outlook.com>
2023-12-07 17:52:18 +0300

    Added - HarfKaristirici

---
+++ OneDArrays/HarfKaristirici/Program.cs
@@ -0,0 +1,10 @@
+﻿// girdi
+Console.Write("Cumle :");
+string sentence = Console.ReadLine();
+string newSentence = "";
+Random random = new Random();
+// harfleri karisitir
+for (int i = sentence.Length; i > 0; i--)
+    newSentence+= sentence[random.Next(i)];
+// cikti
+Console.WriteLine($"Yeni cumle:{newSentence} ");
