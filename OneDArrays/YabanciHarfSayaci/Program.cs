52dc6239ae45e27c7aa080b867c60f9f656bd082
berkaykarakush <berkaykaraks@outlook.com>
2023-12-07 11:09:25 +0300

    Added - YabanciHarfSayaci

---
+++ OneDArrays/YabanciHarfSayaci/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Cumleyi giriniz: ");
+string sentence = Console.ReadLine().ToUpper();
+int count = 0;
+for (int i = 0; i < sentence.Length; i++)
+{
+    int s = sentence[i];
+    if (s >= 90)
+        count++;
+}
+Console.WriteLine($"Yabanci Harf Sayisi: {count}");
