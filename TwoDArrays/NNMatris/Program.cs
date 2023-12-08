e085b5e576a5b6d77f2bfd2cb013cab58b7341e4
berkaykarakush <berkaykaraks@outlook.com>
2023-12-08 09:54:36 +0300

    Added - NNMatris

---
+++ TwoDArrays/NNMatris/Program.cs
@@ -0,0 +1,15 @@
+﻿// girdi
+Console.Write("Matrisinin tipini giriniz :");
+int n = int.Parse(Console.ReadLine());
+// n x n matris tanimlamasi
+int[,] matris = new int[n,n];
+// satir
+for (int i = 0; i < n; i++)
+{
+    // sutun
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"Matris ({i+1},{j+1}): ");
+        matris[i, j] = int.Parse(Console.ReadLine());
+    }
+}
