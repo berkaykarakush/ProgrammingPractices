d1c2f7c839345e6aa81f1e9954adaaaef9553630
berkaykarakush <berkaykaraks@outlook.com>
2023-12-08 17:42:31 +0300

    Added - AltUcgenMatris

---
+++ TwoDArrays/AltUcgenMatris/Program.cs
@@ -0,0 +1,28 @@
+﻿// girdi
+Console.Write("N x N matrisi icin N degeri: ");
+int n = int.Parse(Console.ReadLine());
+// n x n matris tanimla
+int[,] matrix = new int[n, n];
+int[,] matrixT = new int[n, n];
+//satir
+for (int i = 0; i < n; i++)
+{
+    // sutun
+    for (int j = 0; j < n; j++)
+    {
+        //elemanlari al
+        Console.Write($"N({i+1},{j+1}):");
+        matrix[i,j] = int.Parse(Console.ReadLine());
+        // alt ucgen matris icin satir degerinin sutun degerinden fazla olmasi gerekmektedir
+        matrixT[i, j] = i < j ? 0 : matrix[i, j];
+    }
+}
+// satir
+for (int i = 0; i < n; i++)
+{
+    // sutun degerleri
+    for (int j = 0; j < n; j++)
+        Console.Write($"{matrixT[i, j]}\t");
+    // yeni satir
+    Console.WriteLine();
+}
