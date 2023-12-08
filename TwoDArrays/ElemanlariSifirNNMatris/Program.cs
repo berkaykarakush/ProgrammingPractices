163e05491971f9495475851721547e56eeb5211b
berkaykarakush <berkaykaraks@outlook.com>
2023-12-08 10:24:36 +0300

    Added - ElemanlariSifirNNMatris

---
+++ TwoDArrays/ElemanlariSifirNNMatris/Program.cs
@@ -0,0 +1,15 @@
+﻿// girdi
+Console.Write("N x N, N degeri: ");
+int n = int.Parse(Console.ReadLine());
+// n x n matris tanimla
+int[,] matris = new int[n, n];
+// tum degerler default olarak 9 gelmektedir
+// satir
+for (int i = 0; i < n; i++)
+{
+    // sutun
+    for (int j = 0; j < n; j++)
+        Console.Write($"{matris[i, j]}\t");
+    // yeni satira gec
+    Console.WriteLine();
+}
