8ef3f3b9b7eeaabeb165e9999a4f2e0cc39f586e
berkaykarakush <berkaykaraks@outlook.com>
2023-12-08 15:57:12 +0300

    Added - AnaKosegenUstuToplama

---
+++ TwoDArrays/AnaKosegenUstuToplama/Program.cs
@@ -0,0 +1,22 @@
+﻿// girdi
+Console.Write("N x N icin N degeri:");
+int n = int.Parse(Console.ReadLine());
+// n x n matris tanimla
+int[,] matris = new int[n, n];
+int result = 0;
+// satir
+for (int i = 0; i < n; i++)
+{
+    // sutun
+    for (int j = 0; j < n; j++)
+    {
+        // degerleri al
+        Console.Write($"N({i + 1},{j + 1}):");
+        matris[i,j] = int.Parse(Console.ReadLine());
+        // ana kosegen uzerinde kalan elemanlarin satir indisi sutun indisinden kucuktur yani i < j
+        if (i < j)
+            result += matris[i, j];
+    }
+}
+// cikti
+Console.WriteLine($"Result = {result}");
