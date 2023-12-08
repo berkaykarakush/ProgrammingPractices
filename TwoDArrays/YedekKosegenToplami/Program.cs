6337af961ea8abe28d11e01331b5a9f8bca08914
berkaykarakush <berkaykaraks@outlook.com>
2023-12-08 11:43:10 +0300

    Added - YedekKosegenToplami

---
+++ TwoDArrays/YedekKosegenToplami/Program.cs
@@ -0,0 +1,20 @@
+﻿// girdi
+Console.Write("N x N Matrisi icin N degeri:");
+int n = int.Parse(Console.ReadLine());
+int[,] matris = new int[n,n];
+int result = 0;
+// satir
+for (int i = 0; i < n; i++)
+{
+    // sutun
+    for (int j = 0; j < n; j++)
+    {
+        // matris degerlerini al
+        Console.Write($"N({i + 1},{j + 1}) = ");
+        matris[i,j] = int.Parse(Console.ReadLine());
+        //// yedek kosegen hesaplama yani n1, 33, 1n e dogur sol alt kosegenden sag ust koseye
+        if (i + j == n - 1)
+            result += matris[i, j];
+    }
+}
+Console.WriteLine($"Sonuc: {result}");
