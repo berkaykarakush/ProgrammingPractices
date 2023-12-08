4b30ef22219acbd39dd72ab1a56c8fffb0debfb7
berkaykarakush <berkaykaraks@outlook.com>
2023-12-08 10:20:22 +0300

    Added - KareMatrisElemanYazdir

---
+++ TwoDArrays/KareMatrisElemanYazdir/Program.cs
@@ -0,0 +1,25 @@
+﻿// girdi
+Console.Write("Kare matrisin tipini giriniz: ");
+int n = int.Parse(Console.ReadLine());
+int[,] matris = new int[n, n];
+// satir
+for (int i = 0; i < n; i++)
+{
+    // sutun
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"Matris ({i + 1},{j + 1}): ");
+        matris[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+// cikti
+Console.WriteLine("Matris\n----------------------");
+// satir
+for (int i = 0; i < n; i++)
+{
+    // sutun
+    for (int j = 0; j < n; j++)
+        Console.Write($"{matris[i, j]}\t");
+    // sutun bitince yeni satira gec
+    Console.WriteLine();
+}
