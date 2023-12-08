876fb1d7a25e91ecc31a94a3ccfcd6f5b612b374
berkaykarakush <berkaykaraks@outlook.com>
2023-12-08 11:51:27 +0300

    Added - MatrisElemanlarToplami

---
+++ TwoDArrays/MatrisElemanlarToplami/Program.cs
@@ -0,0 +1,21 @@
+﻿// girdi
+Console.Write("N x N matrisi icin N degeri:");
+int n = int.Parse(Console.ReadLine());
+// n x n matris tanimla
+int[,] matris = new int[n, n];
+int result = 0;
+// satir
+for (int i = 0; i < n; i++)
+{
+	// sutun
+	for (int j = 0; j < n; j++)
+	{
+		// elemanlari al
+		Console.Write($"N({i + 1},{j + 1}):");
+		matris[i, j] = int.Parse(Console.ReadLine());
+		// her elemani topla
+		result += matris[i, j];
+	}
+}
+// cikti
+Console.WriteLine($"Toplam: {result}");
