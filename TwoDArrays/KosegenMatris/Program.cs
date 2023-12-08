82cff09bb6932792abbd935f39148a979f559617
berkaykarakush <berkaykaraks@outlook.com>
2023-12-08 17:47:27 +0300

    Added - KosegenMatris

---
+++ TwoDArrays/KosegenMatris/Program.cs
@@ -0,0 +1,29 @@
+﻿// girdi
+Console.Write("N x N matrisi icin N degeri:");
+int n = int.Parse(Console.ReadLine());
+// matris tanimla
+int[,] matrix = new int[n, n];
+int[,] matrixC = new int[n, n];
+// satir
+for (int i = 0; i < n; i++)
+{
+    // sutun
+    for (int j = 0; j < n; j++)
+    {
+        // elemanlari al
+        Console.Write($"N({i+1},{j+1}):");
+        matrix[i,j] = int.Parse(Console.ReadLine());
+        // kosegen olmasi icin ana kosegen degerlerine ihtiyac vardir yani 1,0 2,0 3,0 seklinde
+        if (i == j)
+            matrixC[i, 0] = matrix[i, j];
+    }
+}
+// satir degerleri
+for (int i = 0; i < n; i++)
+{
+    // sutun degerleri
+    for (int j = 0; j < n; j++)
+        Console.Write($"{matrixC[i,j]}\t");
+    // yeni satir
+    Console.WriteLine();
+}
