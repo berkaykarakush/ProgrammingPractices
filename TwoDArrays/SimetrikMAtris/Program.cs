8c48e926e92f5690e79adda132dd4bff61d7c0b7
berkaykarakush <berkaykaraks@outlook.com>
2023-12-09 12:21:02 +0300

    Added - SimetrikMAtris

---
+++ TwoDArrays/SimetrikMAtris/Program.cs
@@ -0,0 +1,33 @@
+﻿// girdi
+Console.Write("N x N matrisi icin N degeri:");
+int n = int.Parse(Console.ReadLine());
+// n x n matris tanimla
+int[,] matrix = new int[n,n];
+bool result = true;
+// satir
+for (int i = 0; i < n; i++)
+{
+    // sutun
+	for (int j = 0; j < n; j++)
+    {
+        // degerleri al
+        Console.Write($"N({i + 1},{j + 1}):");
+        matrix[i,j] = int.Parse(Console.ReadLine());
+    }
+}
+// satir
+for (int i = 0; i < n; i++)
+{
+    // sutun
+    for (int j = 0; j < n; j++)
+    {
+        // esil degilse kontrolu sonlandir
+        if (matrix[i, j] != matrix[j, i])
+        {
+            result = false;
+            break;
+        }
+    }
+}
+// dogru ise simetrik, degilse degil yaz
+Console.WriteLine(result == true ? "Simetrik" : "Simetrik degildir");
