ddcc01c780c7bd4ee27fc483914f33b27160aab2
berkaykarakush <berkaykaraks@outlook.com>
2023-12-11 16:03:48 +0300

    Added - DikUcgenKelime

---
+++ TwoDArrays/DikUcgenKelime/Program.cs
@@ -0,0 +1,7 @@
+﻿// girdi
+Console.Write("Kelime giriniz: ");
+string text = Console.ReadLine();
+// kelimenin icinde dolas
+for (int i = 0; i < text.Length; i++)
+    // her dongude bir harf arttir ve yaz
+    Console.WriteLine(text.Substring(0, i+1));
