8f5cb721fb03a90321ed1f4565264161d2101caa
berkaykarakush <berkaykaraks@outlook.com>
2023-12-11 15:37:36 +0300

    Added - BasamakliIsimYazdirma3

---
+++ TwoDArrays/BasamakliIsimYazdirma3/Program.cs
@@ -0,0 +1,11 @@
+﻿string text = "TORONTO";
+// her karakter icin 
+for (int i = 0; i < text.Length; i++)
+{
+    // belirli bir konuma imleci ayarla
+    Console.SetCursorPosition(i, i);
+    // ilgili karakteri al
+    string t = text.Substring(i,1);
+    // ilk isim / ikinci kisim / ucuncu kisim seklinde cikti olusturur
+    Console.Write($"{t.PadRight(text.Length-i)}  {t.PadRight(text.Length-i)} {t}");
+}
