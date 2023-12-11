070757abd0b1ec4afdb27d93e433539e6893f82d
berkaykarakush <berkaykaraks@outlook.com>
2023-12-11 15:27:58 +0300

    Added - BasamakliIsimYazdirma2

---
+++ TwoDArrays/BasamakliIsimYazdirma2/Program.cs
@@ -0,0 +1,10 @@
+﻿string text = "BURSA";
+for (int i = 0; i < 10; i++)
+{
+    // bastan 2 karakter oteler
+    Console.SetCursorPosition(i,2*i+1);
+    Console.Write(text);
+    // sondan 2 karakter oteler
+    Console.SetCursorPosition(i, 46 - 2 * i);
+    Console.Write(text);
+}
