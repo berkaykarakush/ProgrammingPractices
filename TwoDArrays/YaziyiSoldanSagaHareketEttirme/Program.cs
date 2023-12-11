3dbe3dea382dd929e33bf82f0202a838596bc5b1
berkaykarakush <berkaykaraks@outlook.com>
2023-12-11 16:57:10 +0300

    Added - YaziyiSoldanSagaHareketEttirme

---
+++ TwoDArrays/YaziyiSoldanSagaHareketEttirme/Program.cs
@@ -0,0 +1,15 @@
+﻿// girdi
+Console.Write("Kelime girini: ");
+string text = Console.ReadLine();
+// yaziyi soldan saga oteler 30 kere
+for (int i = 0; i < 30; i++)
+{
+    // pointeri ayni hizara saga oteler
+    Console.SetCursorPosition(i, 1);
+    // yaz
+    Console.Write(text);
+    // 1 sn bekle
+    Thread.Sleep(1000);
+    // temizle
+    Console.Clear();
+}
