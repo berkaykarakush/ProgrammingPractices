9e361e0bc4f9abad24dae2228bb091cd8265e73c
berkaykarakush <berkaykaraks@outlook.com>
2023-12-11 15:10:15 +0300

    Added - BasamakliIsimYazdirma

---
+++ TwoDArrays/BasamakliIsimYazdirma/Program.cs
@@ -0,0 +1,7 @@
+﻿// 10 kez tekrarla
+for (int i = 0; i < 10; i++)
+{
+    // 3 karakter oteleme yapar
+    Console.SetCursorPosition(3 * i, i);
+    Console.Write("BURSA");
+}
--- TwoDArrays/KareMatrisTersi/Program.cs
+++ TwoDArrays/KareMatrisTersi/Program.cs
@@ -1,18 +1,20 @@
 ﻿// girdi
-Console.WriteLine("N x N Matris icin N degeri:");
+Console.Write("N x N Matris icin N degeri:");
 int n = int.Parse(Console.ReadLine());
 // n x n matris tanimla
-int[,] matrix = new int[n, n];
-int[,] reverse = new int[n, n];
+double[,] matrix = new double[n, n];
+double[,] reverse = new double[n, n];
 // satir
 for (int i = 0; i < n; i++)
 {
 	// sutun
 	for (int j = 0; j < n; j++)
 	{
-		// matrisin elemanlarina 1-10 arasi rastgele degerler atanir
-		matrix[i, j] = new Random().Next(1,10);
-		Console.Write($"{matrix[i,j]}\t");
+        // matrisin elemanlarina 1-10 arasi rastgele degerler atanir
+        Console.Write("ELeman: ");
+        matrix[i, j] = int.Parse(Console.ReadLine());
+        //matrix[i, j] = new Random().Next(1,10);
+		//Console.Write($"{matrix[i,j]}\t");
     }
     Console.WriteLine();
 }
@@ -21,22 +23,29 @@ for (int k = 0; k < n; k++)
 {
 	// satir
 	for (int i = 0; i < n; i++)
+	{
 		// sutun
 		for (int j = 0; j < n; j++)
+		{
 			// diagonal eleman disindaki elemanlar guncellenir
-			if ((i != k) || (j!=k))
+			if (!((i == k) || (j == k)))
 				matrix[i, j] -= matrix[i,k] * matrix[k,j] / matrix[k,k];
-
+		}
+	}
 	// diagonal eleman guncellenir
 	matrix[k, k] = -1 / matrix[k, k];
 	// matrisin ust ucgen kismini guncelle
 	for (int i = 0; i < n; i++)
-		if (i == k)
+	{
+		if (i != k)
 			matrix[i, k] *= matrix[k, k];
+	}
 	// matrisin alt ucgen kismi guncellenir
 	for (int j = 0; j < n; j++)
-		if (j == k)
+	{
+		if (j != k)
 			matrix[k, j] *= matrix[k, k];
+	}
 }
 // Ters matris olusturma ve ekrana yazdirma islemleri
 // satir
@@ -48,7 +57,7 @@ for (int i = 0; i < n; i++)
 		// tersini al
 		reverse[i, j] = -matrix[i, j];
 		// ekrana yaz
-		Console.Write($"{reverse[i,j]}");
+		Console.Write($"{reverse[i,j]:F3}\t");
     }
 	// yeni satira gec
     Console.WriteLine();
