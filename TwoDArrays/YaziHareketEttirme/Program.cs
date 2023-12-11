92304316352d33f9bc3dd76e0c1da28173322aa9
berkaykarakush <berkaykaraks@outlook.com>
2023-12-11 16:10:09 +0300

    Added - YaziHareketEttirme

---
+++ TwoDArrays/YaziHareketEttirme/Program.cs
@@ -0,0 +1,15 @@
+﻿// girdi
+Console.Write("Kelime giriniz: ");
+string text = Console.ReadLine();
+// 24 kere islem tekrarla
+for (int i = 24; 1 < i; i--)
+{
+    // pointeri hareket ettir
+    Console.SetCursorPosition(24,i);
+    // metni yaz
+    Console.Write(text);
+    // 1 sn bekle
+    Thread.Sleep(1000);
+    // konsol u temizle
+    Console.Clear();
+}
