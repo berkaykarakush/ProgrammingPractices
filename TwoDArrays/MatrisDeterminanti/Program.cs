e8bbae5d9763a3935d8a9808146eefb8a43f0d4b
berkaykarakush <berkaykaraks@outlook.com>
2023-12-11 11:43:19 +0300

    Added - MatrisDeterminanti

---
+++ TwoDArrays/MatrisDeterminanti/Program.cs
@@ -0,0 +1,20 @@
+﻿// girdi
+Console.Write("N x N matrisi icin N degeri:");
+int n = int.Parse(Console.ReadLine());
+// n x n matris tanimlama
+int[,] matrix = new int[n, n];
+// satir
+for (int i = 0; i < n; i++)
+{
+	// sutun
+	for (int j = 0; j < n; j++)
+	{
+		matrix[i, j] = new Random().Next(1, 10);
+		Console.Write($"{matrix[i,j]}\t");
+    }
+	Console.WriteLine();
+}
+// 2 x 2 icin determinant
+int d = matrix[0,0] * matrix[1,1] - matrix[0,1] * matrix[1,0];
+// cikti
+Console.WriteLine($"Determinant : {d}");
