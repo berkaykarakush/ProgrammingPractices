314ff36fb8b9689dfe8cac17cf4bd66c7ef72f23
berkaykarakush <berkaykaraks@outlook.com>
2023-12-11 14:43:14 +0300

    Added - KareMatrisTersi

---
+++ TwoDArrays/KareMatrisTersi/Program.cs
@@ -0,0 +1,55 @@
+﻿// girdi
+Console.WriteLine("N x N Matris icin N degeri:");
+int n = int.Parse(Console.ReadLine());
+// n x n matris tanimla
+int[,] matrix = new int[n, n];
+int[,] reverse = new int[n, n];
+// satir
+for (int i = 0; i < n; i++)
+{
+	// sutun
+	for (int j = 0; j < n; j++)
+	{
+		// matrisin elemanlarina 1-10 arasi rastgele degerler atanir
+		matrix[i, j] = new Random().Next(1,10);
+		Console.Write($"{matrix[i,j]}\t");
+    }
+    Console.WriteLine();
+}
+// Gauss-Jordan eliminasyon yontemi kullanilarak matrisin tersi alinir
+for (int k = 0; k < n; k++)
+{
+	// satir
+	for (int i = 0; i < n; i++)
+		// sutun
+		for (int j = 0; j < n; j++)
+			// diagonal eleman disindaki elemanlar guncellenir
+			if ((i != k) || (j!=k))
+				matrix[i, j] -= matrix[i,k] * matrix[k,j] / matrix[k,k];
+
+	// diagonal eleman guncellenir
+	matrix[k, k] = -1 / matrix[k, k];
+	// matrisin ust ucgen kismini guncelle
+	for (int i = 0; i < n; i++)
+		if (i == k)
+			matrix[i, k] *= matrix[k, k];
+	// matrisin alt ucgen kismi guncellenir
+	for (int j = 0; j < n; j++)
+		if (j == k)
+			matrix[k, j] *= matrix[k, k];
+}
+// Ters matris olusturma ve ekrana yazdirma islemleri
+// satir
+for (int i = 0; i < n; i++)
+{
+	// sutun
+	for (int j = 0; j < n; j++)
+	{
+		// tersini al
+		reverse[i, j] = -matrix[i, j];
+		// ekrana yaz
+		Console.Write($"{reverse[i,j]}");
+    }
+	// yeni satira gec
+    Console.WriteLine();
+}
