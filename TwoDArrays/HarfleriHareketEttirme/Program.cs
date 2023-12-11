2a3d2d2d7a91f29e07ae4e8df8a0ac9371f3d374
berkaykarakush <berkaykaraks@outlook.com>
2023-12-11 16:57:28 +0300

    Added - HarfleriHareketEttirme

--- TwoDArrays/HarfleriHareketEttirme/Program.cs
+++ TwoDArrays/HarfleriHareketEttirme/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿// girdi
+Console.Write("Kelime giriniz: ");
+string text = Console.ReadLine();
+// konsolu temizle
+Console.Clear();
+// metni ekrana bas
+Console.WriteLine(text);
+// metin icinde don
+for (int i = 0; i < text.Length; i++)
+{
+    // girilen metnin tersini al
+    string reverse = new string(text.Reverse().ToArray());
+    // pointeri hareket ettir
+    Console.SetCursorPosition(i+text.Length, i+1);
+    // eksilen harfi goster
+    Console.WriteLine(reverse[i]);
+    // 1 sn bekle
+    Thread.Sleep(1000);
+}
