b58eab0cf7e2144a4db119e5834a54bf033f82b5
berkaykarakush <berkaykaraks@outlook.com>
2023-12-14 15:25:16 +0300

    Added - KayitEkleme2

---
+++ FileApps/KayitEkleme2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Dosya adi: ");
+string fileName = Console.ReadLine() + ".txt";
+Console.Write("Ad: ");
+string name = Console.ReadLine();
+Console.Write("Telefon: ");
+string phone = Console.ReadLine();
+Console.Write("Adres: ");
+string address = Console.ReadLine();
+
+// Dosya adına ".txt" uzantısını ekleyerek FileStream oluştur
+using (FileStream fileStream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write))
+{
+    // StreamWriter kullanarak dosyaya yazma işlemi
+    using (StreamWriter streamWriter = new StreamWriter(fileStream))
+    {
+        // Kullanıcıdan alınan bilgileri dosyaya yaz
+        streamWriter.WriteLine($"{name} - {phone} - {address}");
+        Console.WriteLine($"Islem tamamlandi {fileStream.Name}");
+    }
+}
