833138198ea83a551867305e49de0103a213788f
berkaykarakush <berkaykaraks@outlook.com>
2023-12-14 15:05:29 +0300

    Added - KayitListeleme2

---
+++ FileApps/KayitListeleme2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Rastgele Erişimli Dosya
+// Kullanıcıdan dosya yolunu alır.
+Console.Write("Dosya yolunu giriniz: ");
+string filePath = Console.ReadLine();
+// FileStream sınıfı, dosya üzerinde işlemler yapmak için kullanılır.
+// Kullanıcının girdiği dosya yolundaki dosyayı açar ve rastgele erişim sağlar.
+FileStream file = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+// BinaryReader sınıfı, rastgele erişimli dosyadan veri okuma işlemleri için kullanılır.
+BinaryReader binaryReader = new BinaryReader(file);
+// Dosyadan veri okuma işlemleri gerçekleştirilir.
+// Bu örnekte, her bir satırın bir önceki satırdan farklı bir formatta olduğunu varsayıyoruz.
+while (binaryReader.PeekChar() != -1)
+{
+    string line = binaryReader.ReadString();
+    Console.WriteLine(line);
+}
+// BinaryReader ve FileStream kapatılır. Dosyayla ilişkili kaynaklar serbest bırakılır.
+binaryReader.Close();
+file.Close();
