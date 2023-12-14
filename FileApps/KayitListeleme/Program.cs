b8d1b60498504e05665229fd73f9365fd82b1e2b
berkaykarakush <berkaykaraks@outlook.com>
2023-12-14 15:02:30 +0300

    Added - KayitListeleme

---
+++ FileApps/KayitListeleme/Program.cs
@@ -0,0 +1,17 @@
+﻿// Sıralı Erişimli Dosya
+// FileStream sınıfı, dosya üzerinde işlemler yapmak için kullanılır.
+// "guide.txt" adlı dosyayı açar ve sadece okuma yetkisi verir.
+FileStream file = new FileStream("guide.txt", FileMode.Open, FileAccess.Read);
+// StreamReader sınıfı, metin okuma işlemleri için kullanılır ve FileStream'e bağlanır.
+StreamReader streamReader = new StreamReader(file);
+// Dosyadan bir satır okunur.
+string line = streamReader.ReadLine();
+// Dosya sonuna kadar satırları okuyarak ekrana yazdırır.
+while (line != null)
+{
+    Console.WriteLine(line);
+    // Bir sonraki satıra geçer.
+    line = streamReader.ReadLine();
+}
+// StreamReader ve FileStream kapatılır. Dosyayla ilişkili kaynaklar serbest bırakılır.
+file.Close();
